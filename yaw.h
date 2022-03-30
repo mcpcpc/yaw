f3290563fa0c6ef8042a555b1c075991a1c7c692
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-03-30 14:58:30 -0400

    Create yaw.h

---
+++ yaw.h
@@ -0,0 +1,25 @@
+#ifndef __YAW_H
+#define __YAW_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <netdb.h>
+
+typedef struct PACKAGE {
+	char *name;
+	char *host;
+	char *path;
+	char *version;
+	char *build;
+	uint32_t checksum;
+} pkg_t, *pkg;
+
+int package_create(pkg package, char *name, char *source_url, char *version);
+int package_destroy(pkg package);
+int package_download(pkg package);
+int package_verify(pkg package);
+int package_print(pkg ppackage);
+
+#endif
